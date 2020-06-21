9ba79bc450de7d8a3895082f5f026d98a530d0d2
Marino <marinogomez24@gmail.com>
2020-06-21 03:47:02 -0400

    More API routes implementation: post a new user, get assignment info, get all the assignments.
    Co-authored-by: Ruben Nunez <ruben_abdias@live.com>

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
+const bcrypt = require("bcrypt");
+const saltRounds = parseInt(process.env.saltRounds);
 const express = require("express");
 const app = express();
+var bodyParser = require("body-parser");
 const uri = process.env.mongodbURI;
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
@@ -9,20 +12,27 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 const application = (err, client) => {
   // Get all users
-  app.get("/", async (request, response) => {
-    const users = await client.db().collection("users").find().toArray();
-    if (users.length) {
-      const first = users[0];
-      return response.send({
-        Nombre: first.Nombre + " " + first.Apellido,
-        Correo: first.Correo,
+  app.get("/", (request, response) => {
+    return client
+      .db()
+      .collection("users")
+      .find()
+      .toArray()
+      .then((users) => {
+        if (users.length) {
+          return response.send(
+            users.map((user) => user.Nombre + " " + user.Apellido)
+          );
+        } else {
+          return response.send("No se encontraron usuarios.");
+        }
       });
-    } else {
-      return response.send("No se encontraron usuarios.");
-    }
   });
+  // Get all students of a classroom
   app.get("/:idAula/students", async (request, response) => {
     /*url params express*/
     const idAula = request.params.idAula;
@@ -39,7 +49,9 @@ const application = (err, client) => {
               .collection("users")
               .find({ _id: ObjectId(student.idUsuario) })
               .toArray()
-              .then((user) => user[0].Nombre + " " + user[0].Apellido)
+              .then((users) =>
+                users.map((user) => user.Nombre + " " + user.Apellido)
+              )
           )
         )
           .then((users) => {
@@ -52,7 +64,96 @@ const application = (err, client) => {
           .catch(console.log)
       );
   });
-  // listen for requests :)
+  // Get assignment info
+  app.get("/:idAula/asignaciones/:_id", async (request, response) => {
+    /*url params express*/
+    const idAula = request.params.idAula;
+    const idAssignment = request.params._id;
+    let assignment = client
+      .db()
+      .collection("asignment")
+      .findOne({ _id: ObjectId(idAssignment), idAula })
+      .then(
+        async ({
+          idRubrica,
+          FechaApertura,
+          FechaEntrega,
+          FechaCierre,
+          Puntuacion,
+        }) => {
+          let rubric = await client
+            .db()
+            .collection("rubric")
+            .findOne({ _id: ObjectId(idRubrica) });
+          console.log(rubric);
+          if (rubric) {
+            const { Nombre, Descripcion } = rubric;
+            let assData = {
+              Nombre,
+              Descripcion,
+              FechaApertura,
+              FechaEntrega,
+              FechaCierre,
+              Puntuacion,
+            };
+            return response.send(assData);
+          } else {
+            return response.send("No se encontraron estudiantes.");
+          }
+        }
+      )
+
+      .catch(console.log);
+  });
+  // Get all the assignments
+  app.get("/:idAula/asignaciones", async (request, response) => {
+    /*url params express*/
+    const idAula = request.params.idAula;
+    let assignments = client
+      .db()
+      .collection("asignment")
+      .find({ idAula })
+      .toArray()
+      .then((assignments) =>
+        Promise.all(
+          assignments.map((assignment) =>
+            client
+              .db()
+              .collection("rubric")
+              .find({ _id: ObjectId(assignment.idRubrica) })
+              .toArray()
+              .then((rubric) => rubric[0].Nombre + ": " + rubric[0].Descripcion)
+          )
+        ).then((rubric) => {
+          if (rubric) {
+            return response.send(rubric);
+          } else {
+            return response.send("No se encontraron estudiantes.");
+          }
+        })
+      )
+      .catch(console.log);
+  });
+  // Register a new user
+  app.post("/user", function (request, response) {
+    const solUsuario = request.body;
+    bcrypt.hash(solUsuario.Password, saltRounds).then((result) => {
+      const postUsuario = {
+        Nombre: solUsuario.Nombre,
+        Apellido: solUsuario.Apellido,
+        Correo: solUsuario.Correo,
+        Username: solUsuario.Username,
+        ClaveEncrypt: result,
+        Descripcion: solUsuario.Descripcion,
+      };
+      return client
+        .db()
+        .collection("users")
+        .insertOne(postUsuario)
+        .then(() => response.send(postUsuario));
+    });
+  });
+  // listen for requests
   const listener = app.listen(process.env.PORT, () => {
     console.log("Your app is listening on port " + listener.address().port);
   });
