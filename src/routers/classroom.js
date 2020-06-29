7b5497d0edd18f1b695107b287f1584e8ac83f52
Marino <marinogomez24@gmail.com>
2020-06-29 02:03:12 -0400

    Adding new Gets to Classroom
    Get all students from my classroom.
    Get all assignments from my classroom.
    
    Co-authored-by: Ruben Nunez <ruben_abdias@live.com>

--- src/routers/classroom.js
+++ src/routers/classroom.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Classroom = require("../models/classroom");
 var bodyParser = require("body-parser");
 const User = require("../models/user");
+const Assignment = require("../models/assignment");
 const auth = require("../middleware/auth");
 const { ObjectId } = require("mongodb");
 const mongoose = require("mongoose");
@@ -67,14 +68,44 @@ router.get("/classrooms", auth, async (req, res) => {
   }
 });
 
-// Returns a list with all students names in the classroom
-router.get("/classrooms/:classroom_id/students", auth, async (req, res) => {
+// Returns a list with all assignments in the classroom
+router.get("/classrooms/:classroom_id/assignments", auth, async (req, res) => {
   const classroom_id = req.params.classroom_id;
-  const students = await Classroom.find({
+  const assignments = await Assignment.find({
     classroom: ObjectId(classroom_id),
   });
   try {
-    res.status(201).send(students);
+    res.status(201).send(assignments);
+  } catch (e) {
+    res.status(400);
+  }
+});
+
+// Returns a list with all students names in the classroom
+router.get("/classrooms/:classroom_id/students", auth, async (req, res) => {
+  const classroom_id = req.params.classroom_id;
+  const students = await Classroom.find(
+    {
+      _id: ObjectId(classroom_id),
+    },
+    ["students.user"]
+  );
+  let students_list = Promise.all(
+    students[0].students.map((student) =>
+      User.find(
+        {
+          _id: student.user,
+        },
+        ["name", "lastName"]
+      )
+    )
+  );
+  try {
+    res.status(201).send(
+      (await students_list).map(([student]) => {
+        return { name: student.name, lastName: student.lastName };
+      })
+    );
   } catch (e) {
     res.status(400);
   }
