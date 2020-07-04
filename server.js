008fb40552eb4a432f83808b8c720ce2846e4c50
Marino <marinogomez24@gmail.com>
2020-07-04 11:14:24 -0400

    Fixed port config.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const assignmentRouter = require("./src/routers/assignment");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 5000
 
 require("./src/db/mongoose");
 
