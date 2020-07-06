f0032f4d0ef464f7d1207b6f77e4ca55ad6f431b
Marino <marinogomez24@gmail.com>
2020-07-06 01:33:24 -0400

    Cors setup enabled

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const userRouter = require("./src/routers/user");
 const classroomRouter = require("./src/routers/classroom");
 const rubricRouter = require("./src/routers/rubric");
 const assignmentRouter = require("./src/routers/assignment");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 require("./src/db/mongoose");
 
+app.use(cors());
 app.use(express.json()); // recognize the incoming Request Object as a JSON Object
 app.use(userRouter);
 app.use(classroomRouter);
